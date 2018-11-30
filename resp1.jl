10b400769c75c368f2468051e0f1b693dc7e16fd
Eric Ford <eford@psu.edu>
2018-11-30 02:29:15 -0500

    Added simple documentation

--- resp1.jl
+++ resp1.jl
@@ -1,5 +1,10 @@
 
-identity_function_int(x::Integer) = x
+"Return the integer that was passed."
+identify_identity_function_int(x::Integer) = x
+
+"Return the float64 value that was passed."
 identity_function_float64(x::Float64) = x
+
+"Return the real value that was passed."
 identity_function_real(x::Real) = x
 
