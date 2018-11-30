f572ed653ccc8046f927cc3630e35ffe39585306
Eric Ford <eford@psu.edu>
2018-11-30 02:36:18 -0500

    fix typeo

--- resp1.jl
+++ resp1.jl
@@ -1,6 +1,6 @@
 
 "Return the integer that was passed."
-identify_identity_function_int(x::Integer) = x
+identity_function_int(x::Integer) = x
 
 "Return the float64 value that was passed."
 identity_function_float64(x::Float64) = x
